0d44dc3e1147fb5e228cbdf9d1f41c96d9541fa5
NehalNabel1 <nehalnabel6464@gmail.com>
2026-04-16 17:35:21 +0200

    vercel deployment v3

--- api/index.js
+++ api/index.js
@@ -1,4 +1,26 @@
+// import app from "../server.js";
+// import { connectDB } from "../config/db.js";
+
+// export default async function handler(req, res) {
+//   // Ensure DB connection before handling requests
+//   await connectDB();
+//   return app(req, res);
+// }
+
 import app from "../server.js";
 import { connectDB } from "../config/db.js";
-await connectDB();
-export default app;
+
+let isConnected = false;
+
+export default async function handler(req, res) {
+  try {
+    if (!isConnected) {
+      await connectDB();
+      isConnected = true;
+    }
+
+    return app(req, res);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
